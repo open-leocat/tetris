e267e894dc18bed857281eafcd51ce8a4be395c2
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-05 21:50:16 +0100

    Add files via upload

--- TetrisPython/engine.py
+++ TetrisPython/engine.py
@@ -10,43 +10,64 @@ class Display:
         GPIO.setup(17, GPIO.OUT) # GPIO 17 / DIN
         GPIO.setup(27, GPIO.OUT) # GPIO 27 / CLK
 
+        # Setze das Display auf einen bekannten Zustand
         GPIO.output(2, 0)
         GPIO.output(2, 1)
 
-    def __command(self, command, data):
+    def __send(self, command, data):
+        # Befehl oder Daten?
         GPIO.output(4, command)
         GPIO.output(3, 0)
 
+        # Sendet ein Byte bitweise und sendet jedes Mal einen Impuls an den Clock-Pin. Auch bekannt als "shiftOut()".
         i = 7
         while (i >= 0):
             GPIO.output(17, data & (1 << i))
-            GPIO.output(27, 1);
-            GPIO.output(27, 0);
+            GPIO.output(27, 1)
+            GPIO.output(27, 0)
 
             i -= 1
 
         GPIO.output(3, 1)
 
+    def __command(self, data):
+        self.__send(0, data)
+
+    def __data(self, data):
+        self.__send(1, data)
+
+    def set_cursor(self, x, y):
+        # Achtung: Es wird nicht überprüft, ob sich die Position innerhalb der Grenzen befindet oder nicht.
+        self.__command(0x80 | x)
+        self.__command(0x40 | y)
+
     def clear(self):
-        pass
+        self.set_cursor(0, 0)
+
+        for value in range(4032):
+            data(0);
+
+        self.set_cursor(0, 0)
 
     def draw(self, x, y, data):
-        self.__command(0, 0b00100001 ); # LCD Extended Commands. You can use 0x21
-        self.__command(0, 0b10110000 ); # Set LCD Vop (Contrast). You can use 0xB0
-        self.__command(0, 0b00000100 ); # Set Temp coefficent. You can use 0x04
-        self.__command(0, 0b00010100 ); # LCD bias mode 1:48. You can use 0x14
-        self.__command(0, 0b00100000 ); # LCD Basic Commands. You can use 0x20
-        self.__command(0, 0b00001100 ); # LCD in normal mode. You can use 0x0C
-        self.__command(1, 0b00011111);
-        self.__command(1, 0b00000101);
-        self.__command(1, 0b00000111);
-        self.__command(1, 0b00000000);
-        self.__command(1, 0b00011111);
-        self.__command(1, 0b00000100);
-        self.__command(1, 0b00011111);
-        self.__command(0, 0b00001101);
-        self.__command(0, 0b10000000);
-        self.__command(1, 0b00000000);
+        self.__command(0x21)
+        self.__command(0xB0)
+        self.__command(0x04)
+        self.__command(0x14)
+        self.__command(0x20)
+        self.__command(0x0C)
+        # self.__command(0b00100001) # LCD Extended Commands. You can use 0x21
+        # self.__command(0b10110000) # Set LCD Vop (Contrast). You can use 0xB0
+        # self.__command(0b00000100) # Set Temp coefficent. You can use 0x04
+        # self.__command(0b00010100) # LCD bias mode 1:48. You can use 0x14
+        # self.__command(0b00100000) # LCD Basic Commands. You can use 0x20
+        # self.__command(0b00001100) # LCD in normal mode. You can use 0x0C
+        self.__data(0b01111111)
+        self.__data(0b01001001)
+        self.__data(0b01001001)
+        self.__data(0b01001001)
+        self.__data(0b01001001)
+        self.__data(0b00000000)
 
         pass
 
