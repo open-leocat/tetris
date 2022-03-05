5dd7e539fcd6eadcc41b3701be3ca223a6c30a3f
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-05 18:55:15 +0100

    Add files via upload

---
+++ TetrisPython/engine.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+
+
+
+class Display:
+    def __init__(self):
+        GPIO.setup(3, GPIO.OUT)  # GPIO 2  / RST
+        GPIO.setup(5, GPIO.OUT)  # GPIO 3  / CE
+        GPIO.setup(7, GPIO.OUT)  # GPIO 4  / DC
+        GPIO.setup(11, GPIO.OUT) # GPIO 17 / DIN
+        GPIO.setup(13, GPIO.OUT) # GPIO 27 / CLK
+
+        GPIO.output(3, 0)
+        GPIO.output(3, 1)
+
+    def __command(self, command, data):
+        GPIO.output(4, command)
+        GPIO.output(3, 0)
+
+        i = 7
+        while (i >= 0):
+            GPIO.output(11, data & (1 << i))
+            GPIO.output(27, 1);
+            GPIO.output(27, 0);
+
+            i -= 1
+
+        GPIO.output(3, 1)
+
+    def clear(self):
+        pass
+
+    def draw(self, x, y, data):
+        __command(0, 0b00100001 ); # LCD Extended Commands. You can use 0x21
+        __command(0, 0b10110000 ); # Set LCD Vop (Contrast). You can use 0xB0
+        __command(0, 0b00000100 ); # Set Temp coefficent. You can use 0x04
+        __command(0, 0b00010100 ); # LCD bias mode 1:48. You can use 0x14
+        __command(0, 0b00100000 ); # LCD Basic Commands. You can use 0x20
+        __command(0, 0b00001100 ); # LCD in normal mode. You can use 0x0C
+        __command(1, 0b00011111);
+        __command(1, 0b00000101);
+        __command(1, 0b00000111);
+        __command(1, 0b00000000);
+        __command(1, 0b00011111);
+        __command(1, 0b00000100);
+        __command(1, 0b00011111);
+        __command(0, 0b00001101);
+        __command(0, 0b10000000);
+        __command(1, 0b00000000);
+
+        pass
+
+    def __del__(self):
+        pass
---
+++ TetrisPython/main.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+
+import engine
+
+display = engine.Display()
+
+display.clear()
+display.draw(0, 0, 0)
