5b1a9cc52d7b6696ff5bfd276357d045f0290ae2
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-06 14:30:35 +0100

    Add files via upload

--- TetrisPython/main.py
+++ TetrisPython/main.py
@@ -6,6 +6,7 @@ GPIO.setmode(GPIO.BCM)
 manager = engine.GameManager(2)
 display = engine.Display()
 
+# Der Zustand für das Menü
 class MenuState(engine.GameState):
     def initalize(self):
         pass
@@ -14,21 +15,28 @@ class MenuState(engine.GameState):
         pass
 menu_state = MenuState()
 
+# Der Zustand für das eigentliche Spiel
 class MainState(engine.GameState):
     def initalize(self):
-        self.x = 0.0
-        print("Initalized!")
+        display.clear()
+
+        # "Score"-Text
+        display.set_cursor_y(5) # S
+        display.draw([0x00, 0x3C, 0x60, 0x3C, 0x0E, 0x4E, 0x3C])
+        display.set_cursor_y(4) # CO
+        display.draw([0x00, 0x78, 0xCD, 0xC1, 0xC1, 0xCD, 0x78])
+        display.set_cursor_y(3) # OR
+        display.draw([0x00, 0xF3, 0x9B, 0x9B, 0x9B, 0x9B, 0xF3])
+        display.set_cursor_y(2) # RE
+        display.draw([0x00, 0xE7, 0x36, 0x37, 0xE6, 0x46, 0x37])
+        display.set_cursor_y(1) # E
+        display.draw([0x00, 0xE0, 0x00, 0xC0, 0x00, 0x00, 0xE0])
 
     def update(self):
-        if self.x > 5.0:
-            print("Finished!")
-            manager.stop()
-
-        print("Updated!")
-
-        self.x += manager.delta
+        pass
 main_state = MainState()
 
+# Der Zustand für den GameOver Bildschirm
 class DefeatState(engine.GameState):
     def initalize(self):
         pass
@@ -40,23 +48,5 @@ defeat_state = DefeatState()
 manager.set_state(main_state)
 manager.start()
 manager.stop()
-
-# display.clear()
-#
-# # S
-# display.set_cursor_y(5)
-# display.draw([0x00, 0x3C, 0x60, 0x3C, 0x0E, 0x4E, 0x3C])
-# # CO
-# display.set_cursor_y(4)
-# display.draw([0x00, 0x78, 0xCD, 0xC1, 0xC1, 0xCD, 0x78])
-# # OR
-# display.set_cursor_y(3)
-# display.draw([0x00, 0xF3, 0x9B, 0x9B, 0x9B, 0x9B, 0xF3])
-# # RE
-# display.set_cursor_y(2)
-# display.draw([0x00, 0xE7, 0x36, 0x37, 0xE6, 0x46, 0x37])
-# # E
-# display.set_cursor_y(1)
-# display.draw([0x00, 0xE0, 0x00, 0xC0, 0x00, 0x00, 0xE0])
 
 GPIO.cleanup()
