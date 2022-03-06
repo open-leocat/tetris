322689b3ed65e66b295431e46346e55c0f04faca
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-06 12:35:13 +0100

    Add files via upload

--- TetrisPython/main.py
+++ TetrisPython/main.py
@@ -15,6 +15,7 @@ display.draw(0b01100000)
 display.draw(0b00111100)
 display.draw(0b00001110)
 display.draw(0b01001110)
+display.draw(0b01111100)
 
 # C
 display.set_cursor(0, 4)
@@ -26,6 +27,7 @@ display.draw(0b11000001)
 display.draw(0b11001101)
 display.draw(0b01111000)
 
+# OR
 display.set_cursor(0, 3)
 display.draw(0b00000000)
 display.draw(0b11110011)
