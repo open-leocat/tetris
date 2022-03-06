263d15db88c03e01b0a26023e7ad89a6fd224eaa
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-06 12:39:26 +0100

    Add files via upload

--- TetrisPython/main.py
+++ TetrisPython/main.py
@@ -15,7 +15,7 @@ display.draw(0b01100000)
 display.draw(0b00111100)
 display.draw(0b00001110)
 display.draw(0b01001110)
-display.draw(0b01111100)
+display.draw(0b00111100)
 
 # C
 display.set_cursor(0, 4)
@@ -36,5 +36,15 @@ display.draw(0b10011011)
 display.draw(0b10011011)
 display.draw(0b10011011)
 display.draw(0b11110011)
+
+# RE
+display.set_cursor(0, 2)
+display.draw(0b00000000)
+display.draw(0b11100111)
+display.draw(0b00110110)
+display.draw(0b00110111)
+display.draw(0b11100110)
+display.draw(0b01000110)
+display.draw(0b00110111)
 
 GPIO.cleanup()
