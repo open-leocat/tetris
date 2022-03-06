28d0b7c958d15cf0deee42e8037d573366b14192
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-06 09:47:13 +0100

    Add files via upload

--- TetrisPython/main.py
+++ TetrisPython/main.py
@@ -6,7 +6,11 @@ GPIO.setmode(GPIO.BCM)
 display = engine.Display()
 
 display.clear()
-display.set_cursor(0, 1)
+display.set_cursor(0, 6)
+display.draw(0b00000000)
 display.draw(0b00111100)
+display.draw(0b01100000)
+display.draw(0b00111100)
+display.draw(0b00001110)
 
 GPIO.cleanup()
