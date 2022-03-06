78fb7f1f14b73f56738a50c34eb3d9783f2c1908
open-leocat <68399149+open-leocat@users.noreply.github.com>
2022-03-06 18:43:33 +0100

    Add files via upload

--- TetrisPython/engine.py
+++ TetrisPython/engine.py
@@ -1,54 +1,6 @@
-import time
 import RPi.GPIO as GPIO
 
 
-
-class GameState:
-    # Wird aufgerufen, wenn der Zustand des Managers auf diesen geändert wird
-    def initialize(self):
-        pass
-
-    # Wird jeden Tick aufgerufen
-    def update(self):
-        pass
-
-class GameManager:
-    # TPS = Ticks per second
-    def __init__(self, tps):
-        self.tps = tps
-        self.delta = 0
-        self.__state = GameState()
-        self.__running = False
-
-    def set_state(self, state):
-        self.__state = state
-        self.__state.initalize()
-
-    def start(self):
-        if self.__running:
-            return
-
-        self.__running = True
-
-        # Game Loop
-        tick_time = 1e9 / self.tps # Berechne Nanosekunden pro Tick
-        self.delta = 0.0
-        last_time = time.time_ns()
-        now_time = 0
-
-        while self.__running:
-            now_time = time.time_ns()
-            self.delta += (now_time - last_time) / tick_time # Berechne Deltazeit
-            last_time = now_time
-
-            if self.delta >= 1: # Wenn Tick vorbei -> Update und setze die Deltazeit zurück
-                self.__state.update()
-                self.delta -= 1
-
-    def stop(self):
-        if not self.__running:
-            return
-        self.__running = False
 
 class Display:
     def __init__(self):
@@ -62,19 +14,26 @@ class Display:
         GPIO.output(2, 0)
         GPIO.output(2, 1)
 
-        self.__command(0b00100001) # Signalisiert dem Display, dass erweiterte LCD-Befehle kommen
-        self.__command(0x10111111) # Setzt den Kontrast
-        self.__command(0b00000100) # Setzt den Temperaturkoeffizient?
-        self.__command(0b00010100) # Setzt den Bias Modus auf 1:48
-        self.__command(0b00100000) # Signalisiert dem Display, dass einfache LCD-Befehle kommen
-        self.__command(0b00001100) # Setzt den das Display auf den normalen Modus
+        # self.__command(0b00100001) # LCD Extended Commands. You can use 0x21
+        # self.__command(0b10110000) # Set LCD Vop (Contrast). You can use 0xB0
+        # self.__command(0b00000100) # Set Temp coefficent. You can use 0x04
+        # self.__command(0b00010100) # LCD bias mode 1:48. You can use 0x14
+        # self.__command(0b00100000) # LCD Basic Commands. You can use 0x20
+        # self.__command(0b00001100) # LCD in normal mode. You can use 0x0C
+
+        self.__command(0b00100001) # Signalisiert dem Display, dass erweiterte LCD-Befehle kommen.
+        self.__command(0xBF)
+        self.__command(0b00000100)
+        self.__command(0b00010100)
+        self.__command(0b00100000)
+        self.__command(0b00001100)
 
     def __send(self, command, data):
         # Befehl oder Daten?
         GPIO.output(4, command)
         GPIO.output(3, 0)
 
-        # Sendet ein Byte bitweise und sendet jedes Mal einen Impuls an den Clock-Pin (Theoretisch hätte man auch die SPI-Pins dafür verwenden können, aber: "Never change a running system")
+        # Sendet ein Byte bitweise und sendet jedes Mal einen Impuls an den Clock-Pin. (Theoretisch hätte man auch die SPI-Pins dafür verwenden können, aber: "Never change a running system")
         i = 7
         while (i >= 0):
             GPIO.output(17, data & (1 << i))
@@ -92,12 +51,10 @@ class Display:
         self.__send(1, data)
 
     def set_cursor(self, x, y):
-        # Setzt den X und Y Ram des Displays
         self.__command(0x80 | x)
         self.__command(0x40 | y)
 
     def clear(self):
-        # Leert das Display
         self.set_cursor(0, 0)
 
         for i in range(4032):
@@ -108,3 +65,6 @@ class Display:
     def draw(self, data):
         for b in data:
             self.__data(b)
+
+    def __del__(self):
+        pass
--- TetrisPython/main.py
+++ TetrisPython/main.py
@@ -13,64 +13,35 @@ display.draw([0x00, 0x3C, 0x60, 0x3C, 0x0E, 0x4E, 0x3C])
 # CO
 display.set_cursor(0, 4)
 display.draw([0x00, 0x78, 0xCD, 0xC1, 0xC1, 0xCD, 0x78])
-# OR
-display.set_cursor(0, 3)
-display.draw([0x00, 0xF3, 0x9B, 0x9B, 0x9B, 0x9B, 0xF3])
-# RE
-display.set_cursor(0, 2)
-display.draw([0x00, 0xE7, 0x36, 0x37, 0xE6, 0x46, 0x37])
-# E
-display.set_cursor(0, 1)
-display.draw([0x00, 0xE0, 0x00, 0xC0, 0x00, 0x00, 0xE0])
+
+# # OR
+# display.set_cursor(0, 3)
+# display.draw(0b00000000)
+# display.draw(0b11110011)
+# display.draw(0b10011011)
+# display.draw(0b10011011)
+# display.draw(0b10011011)
+# display.draw(0b10011011)
+# display.draw(0b11110011)
+#
+# # RE
+# display.set_cursor(0, 2)
+# display.draw(0b00000000)
+# display.draw(0b11100111)
+# display.draw(0b00110110)
+# display.draw(0b00110111)
+# display.draw(0b11100110)
+# display.draw(0b01000110)
+# display.draw(0b00110111)
+#
+# # E
+# display.set_cursor(0, 1)
+# display.draw(0b00000000)
+# display.draw(0b11100000)
+# display.draw(0b00000000)
+# display.draw(0b11000000)
+# display.draw(0b00000000)
+# display.draw(0b00000000)
+# display.draw(0b11100000)
 
 GPIO.cleanup()
-
-
-
-# import RPi.GPIO as GPIO
-# import engine
-#
-# GPIO.setmode(GPIO.BCM)
-#
-# manager = engine.GameManager(2)
-# display = engine.Display()
-#
-#
-# # Der Zustand für das eigentliche Spiel
-# class MainState(engine.GameState):
-#     def initalize(self):
-#         pass
-#         # display.clear()
-#
-#         # "Score"-Text
-#         # display.draw([0b11111111])
-#         # display.set_cursor_y(5) # S
-#         # display.draw([0x00, 0x3C, 0x60, 0x3C, 0x0E, 0x4E, 0x3C])
-#         # display.set_cursor_y(4) # CO
-#         # display.draw([0x00, 0x78, 0xCD, 0xC1, 0xC1, 0xCD, 0x78])
-#         # display.set_cursor_y(3) # OR
-#         # display.draw([0x00, 0xF3, 0x9B, 0x9B, 0x9B, 0x9B, 0xF3])
-#         # display.set_cursor_y(2) # RE
-#         # display.draw([0x00, 0xE7, 0x36, 0x37, 0xE6, 0x46, 0x37])
-#         # display.set_cursor_y(1) # E
-#         # display.draw([0x00, 0xE0, 0x00, 0xC0, 0x00, 0x00, 0xE0])
-#         #
-#
-#     def update(self):
-#         self.x = 0.0
-#
-#
-#         if self.x >= 5.0:
-#             manager.stop()
-#
-#         self.x += manager.delta
-# main_state = MainState()
-#
-# manager.set_state(main_state)
-# # manager.start()
-# # manager.stop()
-#
-# display.clear()
-# display.draw([0b11111111])
-#
-# GPIO.cleanup()
